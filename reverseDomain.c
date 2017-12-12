c48c03b649e432088c269cd2308ca00c6de70ea0
Jazmin Villagra <jvillagra@sodep.com.py>
2017-12-12 12:26:42 -0300

    Ejercicio de Reverse Domain

--- reverseDomain.c
+++ reverseDomain.c
@@ -1,5 +1,5 @@
 /*
- * reverseDomain
+ * reverseDomain.c
  * 
  * Copyright 2017 Jaz <jvillagra@stark>
  * 
@@ -26,21 +26,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-char revertDomain(char *);
-int main(int argc, char **argv){
+void reverseDomain(char *);
+
+int main(int argc, char **argv)
+{
+	char dominio[900];	
+	int valid = 1;
+	printf("Ingrese el nombre de dominio: ");
+	scanf("%s`", dominio);
 	
-	char dominio[900];
+	printf("\n1- Dominio ingresado: %s", dominio);
 	
-	printf("Ingrese el dominio:\n")
-	scanf("%[^\n]",dominio);
-	getc(stdin);
+	for (int i=0; i < strlen(dominio); ++i){
+		if (strncmp(&dominio[i],".",1) == 0){
+			if (i==0 || i == strlen(dominio)-1 ){
+				printf("\nERROR. El dominio no puede empezar ni terminar con un punto");
+				valid = 0;
+				break;
+			}
+			
+			if (strncmp(&dominio[i+1],".",1) == 0){
+				printf("\nERROR. El dominio no puede tener dos puntos seguidos");
+				valid = 0;
+				break;
+			}
+		}
+	}
+	if(valid == 1)
+		reverseDomain(dominio);
 	
-	revertDomain(dominio);
-	
-	printf("Reverse Domain: %s",dominio);
 	return 0;
 }
 
-char revertDomain(char *dom){
+void reverseDomain(char * dominio){
 	
+	char copy[900];
+	char * pack;
+	char rev1[900] = "";
+	char rev2[900] = "";
+	int b = 0;
+	
+	//printf("\n HOLA ENTRE A LA FUNCION CON LA VARIABLE DOMINIO = %s", dominio);
+	
+	strcpy(copy,dominio);
+	
+	pack = strtok(copy,".");
+	
+	while(pack != NULL){
+		if(b == 0){
+			strcpy(rev1, pack);
+			strcat(rev1,".");
+			strcat(rev1, rev2);
+			b = 1;
+		}else{
+			strcpy(rev2, pack);
+			strcat(rev2,".");
+			strcat(rev2,rev1);
+			b = 0;
+		}
+		pack = strtok(NULL, ".");
+	}
+	if( b==1){
+		rev1[strlen(rev1)-1]='\0';
+		printf("\n2- Reversed: %s", rev1);
+	}else{
+		rev2[strlen(rev2)-1]='\0';
+		printf("\n2- Reversed: %s",rev2);
+	}
 }
